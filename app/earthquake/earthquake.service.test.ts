4775769b003885251a197d5615991fce0857df6e
sextos3 <sextos3@nationwide.com>
2018-02-27 13:50:39 -0500

    Refactor to clean up tests

--- app/earthquake/earthquake.service.test.ts
+++ app/earthquake/earthquake.service.test.ts
@@ -35,105 +35,106 @@ describe(`EarthquakeServiceTests`, () => {
     });
   });
 
-  it('_mapEarthquakes should map earthquake data back to Earthquake objects', () => {
-    let earthquakeFeatures: EarthquakeFeature[] = [mockEarthquakeFeature()];
-
-    mockResponse.json = jasmine.createSpy('mockResponse.json').and.returnValue({
-      features: earthquakeFeatures
+  describe('_mapEarthquakes', () => {
+    afterEach(() => {
+      expect(mockResponse.json).toHaveBeenCalled();
     });
 
-    expect(service._mapEarthquakes(mockResponse, -1)).toEqual([{
-      usgsTitle: earthquakeFeatures[0].properties.title,
-      time: earthquakeFeatures[0].properties.time,
-      latitude: earthquakeFeatures[0].geometry[0],
-      longitude: earthquakeFeatures[0].geometry[1],
-      magnitude: earthquakeFeatures[0].properties.mag,
-      url: earthquakeFeatures[0].properties.url
-    }]);
+    it('should map earthquake data back to Earthquake objects', () => {
+      let earthquakeFeatures: EarthquakeFeature[] = [mockEarthquakeFeature()];
 
-    expect(mockResponse.json).toHaveBeenCalled();
-  });
+      mockResponse.json = jasmine.createSpy('mockResponse.json').and.returnValue({
+        features: earthquakeFeatures
+      });
 
-  it('_mapEarthquakes should map multiple earthquake data back to Earthquake objects', () => {
-    let earthquakeFeatures: EarthquakeFeature[] = [
-      mockEarthquakeFeature(),
-      {
-        geometry: [6, 12], properties: {
-          mag: 18,
-          title: 'title2',
-          time: 24,
-          url: 'url2'
-        }
-      },
-      {
-        geometry: [7, 14], properties: {
-          mag: 21,
-          title: 'title3',
-          time: 28,
-          url: 'url3'
-        }
-      }
-    ];
-
-    mockResponse.json = jasmine.createSpy('mockResponse.json').and.returnValue({
-      features: earthquakeFeatures
-    });
-
-    expect(service._mapEarthquakes(mockResponse, -1)).toEqual([
-      {
+      expect(service._mapEarthquakes(mockResponse, -1)).toEqual([{
         usgsTitle: earthquakeFeatures[0].properties.title,
         time: earthquakeFeatures[0].properties.time,
         latitude: earthquakeFeatures[0].geometry[0],
         longitude: earthquakeFeatures[0].geometry[1],
         magnitude: earthquakeFeatures[0].properties.mag,
         url: earthquakeFeatures[0].properties.url
-      },
-      {
-        usgsTitle: earthquakeFeatures[1].properties.title,
-        time: earthquakeFeatures[1].properties.time,
-        latitude: earthquakeFeatures[1].geometry[0],
-        longitude: earthquakeFeatures[1].geometry[1],
-        magnitude: earthquakeFeatures[1].properties.mag,
-        url: earthquakeFeatures[1].properties.url
-      },
-      {
-        usgsTitle: earthquakeFeatures[2].properties.title,
-        time: earthquakeFeatures[2].properties.time,
-        latitude: earthquakeFeatures[2].geometry[0],
-        longitude: earthquakeFeatures[2].geometry[1],
-        magnitude: earthquakeFeatures[2].properties.mag,
-        url: earthquakeFeatures[2].properties.url
-      }
-    ]);
-
-    expect(mockResponse.json).toHaveBeenCalled();
-  });
-
-  it('_mapEarthquakes should map earthquake data back to Earthquake objects and limit number returned', () => {
-    let earthquakeFeatures: EarthquakeFeature[] = [
-      mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
-      mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
-      mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
-      mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
-      mockEarthquakeFeature()
-    ];
-
-    mockResponse.json = jasmine.createSpy('mockResponse.json').and.returnValue({
-      features: earthquakeFeatures
+      }]);
     });
 
-    expect(service._mapEarthquakes(mockResponse, -1).length).toBe(EarthquakeService.NUMBER_TO_DISPLAY);
-    expect(mockResponse.json).toHaveBeenCalled();
-  });
+    it('should map multiple earthquake data back to Earthquake objects', () => {
+      let earthquakeFeatures: EarthquakeFeature[] = [
+        mockEarthquakeFeature(),
+        {
+          geometry: [6, 12], properties: {
+            mag: 18,
+            title: 'title2',
+            time: 24,
+            url: 'url2'
+          }
+        },
+        {
+          geometry: [7, 14], properties: {
+            mag: 21,
+            title: 'title3',
+            time: 28,
+            url: 'url3'
+          }
+        }
+      ];
 
-  function mockEarthquakeFeature(): EarthquakeFeature {
-    return {
-      geometry: [5, 10], properties: {
-        mag: 15,
-        title: 'title',
-        time: 20,
-        url: 'url'
-      }
-    };
-  }
+      mockResponse.json = jasmine.createSpy('mockResponse.json').and.returnValue({
+        features: earthquakeFeatures
+      });
+
+      expect(service._mapEarthquakes(mockResponse, -1)).toEqual([
+        {
+          usgsTitle: earthquakeFeatures[0].properties.title,
+          time: earthquakeFeatures[0].properties.time,
+          latitude: earthquakeFeatures[0].geometry[0],
+          longitude: earthquakeFeatures[0].geometry[1],
+          magnitude: earthquakeFeatures[0].properties.mag,
+          url: earthquakeFeatures[0].properties.url
+        },
+        {
+          usgsTitle: earthquakeFeatures[1].properties.title,
+          time: earthquakeFeatures[1].properties.time,
+          latitude: earthquakeFeatures[1].geometry[0],
+          longitude: earthquakeFeatures[1].geometry[1],
+          magnitude: earthquakeFeatures[1].properties.mag,
+          url: earthquakeFeatures[1].properties.url
+        },
+        {
+          usgsTitle: earthquakeFeatures[2].properties.title,
+          time: earthquakeFeatures[2].properties.time,
+          latitude: earthquakeFeatures[2].geometry[0],
+          longitude: earthquakeFeatures[2].geometry[1],
+          magnitude: earthquakeFeatures[2].properties.mag,
+          url: earthquakeFeatures[2].properties.url
+        }
+      ]);
+    });
+
+    it('should map earthquake data back to Earthquake objects and limit number returned', () => {
+      let earthquakeFeatures: EarthquakeFeature[] = [
+        mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
+        mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
+        mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
+        mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(), mockEarthquakeFeature(),
+        mockEarthquakeFeature()
+      ];
+
+      mockResponse.json = jasmine.createSpy('mockResponse.json').and.returnValue({
+        features: earthquakeFeatures
+      });
+
+      expect(service._mapEarthquakes(mockResponse, -1).length).toBe(EarthquakeService.NUMBER_TO_DISPLAY);
+    });
+
+    function mockEarthquakeFeature(): EarthquakeFeature {
+      return {
+        geometry: [5, 10], properties: {
+          mag: 15,
+          title: 'title',
+          time: 20,
+          url: 'url'
+        }
+      };
+    }
+  });
 });
